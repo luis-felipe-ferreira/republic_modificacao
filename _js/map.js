f04e9869690eb4741af3c4322ef7f60872ec4f0f
Your Name <you@example.com>
2025-04-17 20:31:49 -0300

    página de apartamento

---
+++ _js/map.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const map = L.map('map').setView([-2.9057, -41.7754], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+    L.marker([-2.9057, -41.7754]).addTo(map)
+      .bindPopup('Condomínio em Parnaíba')
+      .openPopup();
+  });
+  
