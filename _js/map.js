e297edbabb030d0d5eefbb82dc83b63a940305b0
Francisco Neto <netofrancisco.pro@gmail.com>
2025-04-17 22:25:38 -0300

    formatação

--- _js/map.js
+++ _js/map.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const map = L.map('map').setView([-2.9057, -41.7754], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
-    L.marker([-2.9057, -41.7754]).addTo(map)
-      .bindPopup('Condomínio em Parnaíba')
-      .openPopup();
-  });
-  
+  const map = L.map('map').setView([-2.9057, -41.7754], 13);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+  }).addTo(map);
+  L.marker([-2.9057, -41.7754]).addTo(map)
+    .bindPopup('Condomínio em Parnaíba')
+    .openPopup();
+});
